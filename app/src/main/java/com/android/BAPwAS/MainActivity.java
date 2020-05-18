e43a375f2d45e95a6b5d68b34c75efe6190cbd36
thomas <thomas.bacher@web.de>
2020-05-18 23:09:59 +0200

    2nd Sample - Intent with returning result

--- app/src/main/java/com/android/BAPwAS/MainActivity.java
+++ app/src/main/java/com/android/BAPwAS/MainActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+        int request_Code = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void onClick(View view) {
-        startActivity(new Intent(this, SecondActivity.class));
+        startActivityForResult(new Intent("com.android.BAPwAS.usingintent.SecondActivity"),request_Code);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this,data.getData().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/android/BAPwAS/SecondActivity.java
+++ app/src/main/java/com/android/BAPwAS/SecondActivity.java
@@ -2,8 +2,10 @@ package com.android.BAPwAS;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
@@ -12,6 +14,13 @@ public class SecondActivity extends Activity {
     }
 
     public void onClick(View view) {
-        startActivity(new Intent(this, MainActivity.class));
+        Intent data = new Intent();
+//---get the EditText view---
+        EditText txt_username = (EditText)findViewById(R.id.txtUsername);
+//---set the data to pass back---
+        data.setData(Uri.parse( txt_username.getText().toString()));
+        setResult(RESULT_OK, data);
+//---closes the activity---
+        finish();
     }
 }
